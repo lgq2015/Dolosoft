7909b6149a6b06be2e58f36723689f4a67d8723e
Ander Moran <andermoran@me.com>
2019-08-29 13:39:40 -0500

    Fixed problem installing tweaks with special characters in app's display name
    Tweaks for apps with special characters in their name did not work because theos does not allow project names to have special characters. This is fixed because displayNameLowercaseNoSpace now removes special characters like é.

--- Dolosoft/AMApp.m
+++ Dolosoft/AMApp.m
@@ -14,7 +14,11 @@
     self = [super init];
     if (self) {
         self.displayName = displayName;
-        self.displayNameLowercaseNoSpace = [[displayName stringByReplacingOccurrencesOfString:@" " withString:@""] lowercaseString];
+        // We do this to remove any accents from the app name like "Pokémon"
+        // https://stackoverflow.com/questions/10932405/nsstring-easy-way-to-remove-utf-8-accents-from-a-string
+        NSMutableString *displayNameLowercaseNoSpace = [NSMutableString stringWithString:[[displayName stringByReplacingOccurrencesOfString:@" " withString:@""] lowercaseString]];
+        CFStringTransform((__bridge CFMutableStringRef)displayNameLowercaseNoSpace, NULL, kCFStringTransformStripCombiningMarks, NO);
+        self.displayNameLowercaseNoSpace = [NSString stringWithString:displayNameLowercaseNoSpace];
         self.executableName = executableName;
         self.bundleIdentifier = bundleIdentifier;
         self.pathToDir = pathToBundleDir;
@@ -24,14 +28,9 @@
         self.headerPath = [NSString stringWithFormat:@"%@/%@ headers",
                            [fileManager headersDirectoryPath],
                            displayName];
-        // We do this to remove any accents from the app name like "Pokémon"
-        // https://stackoverflow.com/questions/10932405/nsstring-easy-way-to-remove-utf-8-accents-from-a-string
-        NSMutableString *tweakDirPath = [NSMutableString stringWithFormat:@"%@/dolosoft%@",
-                                         [fileManager tweaksDirectoryPath],
-                                         self.displayNameLowercaseNoSpace];
-        CFStringTransform((__bridge CFMutableStringRef)tweakDirPath, NULL, kCFStringTransformStripCombiningMarks, NO);
-        
-        self.tweakDirPath = [NSString stringWithString:tweakDirPath];
+        self.tweakDirPath = [NSString stringWithFormat:@"%@/dolosoft%@",
+                             [fileManager tweaksDirectoryPath],
+                             self.displayNameLowercaseNoSpace];
         self.tweakFilePath = [NSString stringWithFormat:@"%@/Tweak.x", self.tweakDirPath];
     }
     return self;
